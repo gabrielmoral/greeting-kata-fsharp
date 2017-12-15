c558f42952614c186e854ff7fc177f8d469eeb72
Gabriel Moral <gabrielmoralortiz@gmail.com>
2017-12-15 10:03:20 +0000

    Hello Gabriel

--- Tests.fs
+++ Tests.fs
@@ -3,6 +3,9 @@ module Tests
 open System
 open Xunit
 
+let greet =
+    sprintf "Hello, %s"
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Hello name`` () =
+    Assert.Equal("Hello, Gabriel",greet("Gabriel"))
