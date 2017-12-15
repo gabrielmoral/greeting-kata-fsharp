b10407c19dfa144097a50efc22d08de281a78a06
Gabriel Moral <gabrielmoralortiz@gmail.com>
2017-12-15 11:58:11 +0000

    HELLO, GABRIEL

--- Tests.fs
+++ Tests.fs
@@ -3,14 +3,38 @@ module Tests
 open System
 open Xunit
 
-let greet name =
-    let name' = match name with
-                |"" -> "my friend"
-                | _ -> name
+let private isUpper (name:string) =
+    let rec checkUpper isUpper arrayName =
+        match arrayName with
+        | [] -> isUpper
+        | _ -> if Char.IsUpper arrayName.Head then checkUpper true arrayName.Tail
+                else false
+    
+    checkUpper false (name.ToCharArray() |> Array.toList)
 
-    sprintf "Hello, %s" name'
+
+let shoutGreeting greeting name = 
+    let (greeting:string) = greeting name
+    greeting.ToUpper()
+
+let greet' greeting name = 
+    match isUpper name with
+    |true ->  shoutGreeting greeting name
+    |false -> greeting name
+
+let greet name =
+    let greeting = sprintf "Hello, %s"
+
+    match name with
+    |"" -> greet' greeting "my friend"
+    |_ -> greet' greeting name 
+
 
 [<Fact>]
 let ``Hello name`` () =
     Assert.Equal("Hello, Gabriel",greet("Gabriel"))
     Assert.Equal("Hello, my friend",greet(""))
+
+[<Fact>]
+let ``Shout greeting`` () =
+    Assert.Equal("HELLO, GABRIEL",greet("GABRIEL"))
